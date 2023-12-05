9e1cecec47a82962b1fee265d26a1b7e477001c6
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-12-05 16:47:13 +0100

    Update test

--- tests/testthat/test-dxgap.R
+++ tests/testthat/test-dxgap.R
@@ -1,3 +1,14 @@
+cases <- data.frame(
+  estimated = c(100, 80, 100),
+  notified = c(10, 60, 70)
+)
+
 test_that("dxgap", {
-  expect_equal(1, 1)
+  expect_equal(
+    dxgap(cases, estimated, notified)$dxgap,
+    c(90, 25, 30)
+  )
+  expect_true(
+    is.numeric(dxgap(cases, estimated, notified)$dxgap)
+  )
 })
